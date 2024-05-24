cf4dc14d147ad53e77c9bd58de18448a9b79e03c
Gabrielparizet <parizetgabriel@gmail.com>
2024-05-24 11:40:38 +0200

    feat(accounts): Create accounts table migration and ecto schema

---
+++ lib/book_my_gigs/accounts/account.ex
@@ -0,0 +1,16 @@
+defmodule BookMyGigs.Accounts.Account do
+  @moduledoc """
+  The Ecto Schema for an Account
+  """
+
+  use Ecto.Schema
+
+  @schema_prefix "public"
+  @primary_key {:id, Ecto.UUID, autogenerate: true}
+  schema "accounts" do
+    field(:email, :string)
+    field(:hash_password, :string)
+
+    timestamps(type: :utc_datetime)
+  end
+end
---
+++ priv/repo/migrations/20240524092741_create_accounts_table.exs
@@ -0,0 +1,15 @@
+defmodule BookMyGigs.Repo.Migrations.CreateAccountsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:accounts, primary_key: false, prefix: "public") do
+      add(:id, :uuid, primary_key: true)
+      add(:email, :string)
+      add(:hash_password, :string)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create unique_index(:accounts, [:id, :email])
+  end
+end
