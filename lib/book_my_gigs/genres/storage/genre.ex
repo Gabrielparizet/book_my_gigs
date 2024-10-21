de37c9833be8c8b358c782875856878de1c57400
Gabriel Parizet <parizetgabriel@gmail.com>
2024-10-21 15:22:14 +0200

    feat(genres): genres table migration and ecto.schema

--- lib/book_my_gigs/genres/storage/genre.ex
+++ lib/book_my_gigs/genres/storage/genre.ex
@@ -1,6 +1,6 @@
-defmodule BookMyGigs.Locations.Storage.Location do
+defmodule BookMyGigs.Genres.Storage.Genre do
   @moduledoc """
-  The Ecto Schema for a location
+  The Ecto Schema for a genre
   """
 
   use Ecto.Schema
@@ -8,17 +8,16 @@ defmodule BookMyGigs.Locations.Storage.Location do
 
   @schema_prefix "public"
   @primary_key {:id, Ecto.UUID, autogenerate: true}
-  schema "locations" do
+  schema "genres" do
     field(:name, :string)
 
     timestamps(type: :utc_datetime)
   end
 
-  def changeset(location, attrs) do
-    location
+  def changeset(genre, attrs) do
+    genre
     |> cast(attrs, [:name])
     |> validate_required([:name])
     |> unique_constraint(:name)
   end
-
 end
