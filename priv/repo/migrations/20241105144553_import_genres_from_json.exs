36ab8d426d7491a66783f255dfdafb43032ac28e
Gabriel Parizet <parizetgabriel@gmail.com>
2024-11-05 16:05:55 +0100

    feat(genres): import musical genres from csv

---
+++ priv/repo/migrations/20241105144553_import_genres_from_json.exs
@@ -0,0 +1,25 @@
+defmodule BookMyGigs.Repo.Migrations.ImportGenresFromJson do
+  use Ecto.Migration
+
+  def up do
+    execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+
+    genres_json = "priv/data/genres/genres_list.json"
+    |> File.read!()
+    |> Jason.decode!()
+    |> Enum.each(fn %{"id" => _id, "name" => name} ->
+      query = """
+      INSERT INTO public.genres
+      (id, name, inserted_at, updated_at)
+      VALUES (uuid_generate_v4(), '#{name}', NOW(), NOW())
+      ON CONFLICT (name) DO NOTHING
+      """
+
+      execute(query)
+    end)
+  end
+
+  def down do
+    execute("TRUNCATE public.genres")
+  end
+end
